9b796c516a3ddba7a623db146305ac7d3b2729a5
Rudzani Magoro <magororj@gmail.com>
2018-06-29 09:51:29 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -27,4 +29,7 @@ import { HomePage } from '../pages/home/home';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+  
+  
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,106 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+
+  name="";
+  summary :number;
+  quantity="";
+  
+  whippcream: boolean;
+  chocolate: boolean;
+  price = 25;
+  increment =0;
+  checked = false;
+  
+  value1: string ;
+  value2: string;
+  message;
+  
+  total = 0;
+  
+ 
 
   constructor(public navCtrl: NavController) {
+    this.summary = 0;
 
-  }
+      }
+      // topping(){
+        
 
-}
+      //     this.name;
+      //     if(this.checked=true){
+      //      this.value1="+Whipped Cream";
+      //     }
+      //     if(this.checked=true){
+      //      this.value2="+Chocolate";
+      //     }
+      //     this.increment;
+      //     this.total = this.price * this.increment;
+      //     this.message;
+         
+         
+      // }
+      Order(){
+       if(this.chocolate == true){
+         this.value1= "chocolate";
+         console.log(this.chocolate);
+       }
+        // this.value1= "+Whipped Cream";
+        if(this.whippcream == true){
+        this.value2= "whippcream";
+        console.log(this.whippcream);
+      }
+        this.total = this.price * this.increment;
+        
+        this.summary = 1;
+        
+
+
+        // this.name;
+        // if(this.checked=true){
+        //  this.value1="+Whipped Cream";
+        // }
+        // if(this.checked=true){
+        //  this.value2="+Chocolate";
+        // }
+        // this.increment;
+        // this.total = this.price * this.increment;
+        // this.message;
+       
+       }
+      Reset(){
+ 
+        this.increment = 0;
+        this.value1=" ";
+        this.value2="";
+       
+        this.name=" ";
+        this.total =0;
+        this.quantity;
+              
+        this.price = 0;
+        
+        }
+        
+      
+      
+        clickadd(){
+ 
+          if(this.increment >= 0){
+            this.increment+=1;
+          }
+    
+    }
+    clicksub(){
+
+      if(this.increment >= 1){
+        this.increment -=1;
+      }
+    
+    }
+
+    
+    }
+
+
